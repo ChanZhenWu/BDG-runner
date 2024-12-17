ceb6a854461b573762d27e4d7a932a3a7a6614d8
Noon Chen <125548538+ChanZhenWu@users.noreply.github.com>
2024-12-17 16:29:39 +0800

    v2.5

--- BDG_runner.pl
+++ BDG_runner.pl
@@ -2,32 +2,28 @@
 #################################################################
 ## This script is used to calculate CPK base on 3070 log file.
 ## Author: noon_chen@apple.com
-## V2.4
+## V2.5
 #################################################################
 
-#use strict;
-#use Spreadsheet::ParseExcel;
-#use Spreadsheet::WriteExcel;
-#use Spreadsheet::XLSX;
-#use Spreadsheet::ParseXLSX;
 use Excel::Writer::XLSX;
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 
 #创建一个新的Excel文件
-#my $log_report = Spreadsheet::WriteExcel->new('CPK_report.xls');
-my $log_report = Excel::Writer::XLSX->new('CPK_report.xlsx');
+my $log_report = Excel::Writer::XLSX->new('CPK_report'."-".$hour.$min.$sec.'.xlsx');
 
 #添加一个工作表
 my $summary = $log_report-> add_worksheet('Summary');
 my $workbook = $log_report-> add_worksheet('CPK_report');
 
-$workbook-> freeze_panes(1,11);			#冻结行、列
-$workbook-> set_column(0,0,20);			#设置列宽
-$summary-> set_column(0,5,20);			#设置列宽
-$workbook-> set_row(0,20);				#设置行高
-$summary-> activate();					#设置初始可见
-#$workbook-> protect("drowssap");		#设置密码
-$workbook->set_header('&CUpdated at &D &T');	#设置页脚
-$workbook->set_landscape();				#设置横排格式
+$workbook-> freeze_panes(1,11);			# 冻结行、列
+$workbook-> set_column(0,0,20);			# 设置列宽
+$summary-> set_column(0,5,20);			# 设置列宽
+$workbook-> set_row(0,20);				# 设置行高
+$summary-> activate();					# 设置初始可见
+#$workbook-> protect("drowssap");		# 设置密码
+$workbook->set_header('&CUpdated at &D &T');	# 设置页脚
+$workbook->set_landscape();				# 设置横排格式
+$log_report->set_size(1680, 1180);		# 设置初始窗口尺寸
 
 
 #新建一个格式
@@ -38,8 +34,6 @@ $format_Fcpk = $log_report-> add_format(align=>'center', border=>1, bg_color=>'o
 $format_Pcpk = $log_report-> add_format(bold=>0, align=>'center', border=>1, bg_color=>'lime');
 $format_Hcpk = $log_report-> add_format(bold=>0, align=>'center', border=>1, bg_color=>'yellow');
 $format_FPY  = $log_report-> add_format(align=>'center', border=>1, num_format=> '10');
-
-
 
 #写入文件头
 $row = 0; $col = 0;
@@ -87,9 +81,9 @@ $workbook-> write($row, $col, 'HiLimit', $format_head);
 $row = 0; $col = 4;
 $workbook-> write($row, $col, 'LowLimit', $format_head);
 $row = 0; $col = 5;
-$workbook-> write($row, $col, 'Max', $format_head);
+$workbook-> write($row, $col, 'Max (Marginal)', $format_head);
 $row = 0; $col = 6;
-$workbook-> write($row, $col, 'Min', $format_head);
+$workbook-> write($row, $col, 'Min (Marginal)', $format_head);
 $row = 0; $col = 7;
 $workbook-> write($row, $col, 'Average', $format_head);
 $row = 0; $col = 8;
@@ -124,7 +118,6 @@ $workbook-> conditional_formatting('J2:K9999',
      	value    => 0,
      	format   => $format_Fcpk,
     });
-
 
 $workbook-> write_formula(1, 5, "=MAX(L2:AAA2)", $format_data);  		#输出Max
 $workbook-> write_formula(1, 6, "=MIN(L2:AAA2)", $format_data);			#输出Min
@@ -168,71 +161,202 @@ $log_counter++;
     	elsif ($line =~ "\@BLOCK")
        	{
        		$col = 0;
-       		#2.2# $headN = substr ($line, 8, length($line)-3-8),"\n";  #生成主测试名
        		$headN = $string[1];
        		#print "\n".$headN;
        		#$workbook-> write($row, $col, $headN, $format);
        		#$row++;
        		}
 
-        #2.2# elsif ($line =~ "\@LIM2" and length($line) == 59)
-        elsif ($line =~ "\@LIM2" and scalar @string == 5)
+        elsif ($line =~ "\@LIM2" and $line =~ "\@A-JUM")    # Jumper
        	{
        		#print $headN, "\r";
        		print NLog $headN, "\r";
        		$workbook-> write($row, $col, $headN, $format_item);					#输出测试名，单项测试
 			$workbook-> write($row, 1, substr($line,4,3), $format_data);			#输出TYPE
-       		#2.2# $workbook-> write($row, 3, substr($line,30,13), $format_data);	#输出上限值
+       		$workbook-> write($row, 3, $string[3], $format_data);					#输出上限值
+       		$workbook-> write($row, 4, substr($string[4],0,13), $format_data);		#输出下限值
+			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Max
+			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Min
+			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Average
+			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  #输出标准差
+			$workbook-> write_formula($row, 9, "=IF(I".($row+1).">0,(D".($row+1)."-E".($row+1).")/6/I".($row+1).")", $format_data);  #输出CP
+			$workbook-> write_formula($row, 10, "=MIN((D".($row+1)."-H".($row+1)."),(H".($row+1)."-E".($row+1)."))/I".($row+1)."/3", $format_data);  #输出CPK
+
+       		$workbook-> conditional_formatting($row, 5,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'greater than',
+    			 	value    => "=D".($row+1)."*0.75",
+    			 	format   => $format_Fcpk,
+    			});
+       		$row++;
+       		}
+        elsif ($line =~ "\@LIM2" and $line =~ "\@A-DIO" and scalar @string == 5)    # Diode
+       	{
+       		#print $headN, "\r";
+       		print NLog $headN, "\r";
+       		$workbook-> write($row, $col, $headN, $format_item);					# 输出测试名，单项测试
+			$workbook-> write($row, 1, substr($line,4,3), $format_data);			# 输出TYPE
        		$workbook-> write($row, 3, $string[3], $format_data);
-       		#2.2# $workbook-> write($row, 4, substr($line,44,13), $format_data);	#输出下限值
        		$workbook-> write($row, 4, substr($string[4],0,13), $format_data);
-			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Max
-			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Min
-			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Average
-			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  #输出标准差
+			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Max
+			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Min
+			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Average
+			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出标准差
 			$workbook-> write_formula($row, 9, "=IF(I".($row+1).">0,(D".($row+1)."-E".($row+1).")/6/I".($row+1).")", $format_data);  #输出CP
 			$workbook-> write_formula($row, 10, "=MIN((D".($row+1)."-H".($row+1)."),(H".($row+1)."-E".($row+1)."))/I".($row+1)."/3", $format_data);  #输出CPK
+
+       		$workbook-> conditional_formatting($row, 5,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'greater than',
+    			 	value    => "=D".($row+1)."-(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+
+       		$workbook-> conditional_formatting($row, 6,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'less than',
+    			 	value    => "=E".($row+1)."+(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+    		
        		$row++;
        		}
-
-        #2.2# elsif ($line =~ "\@LIM3" and length($line) == 73)
-        elsif ($line =~ "\@LIM3" and scalar @string == 6)
-       	{
-       		#print $headN, "\r";
-       		print NLog $headN, "\r";
-       		$workbook-> write($row, $col, $headN, $format_item);					#输出测试名，单项测试
-			$workbook-> write($row, 1, substr($line,4,3), $format_data);			#输出TYPE
-       		$workbook-> write($row, 2, substr($line,index($line,"\@LIM")+6,13), $format_data);  #输出正常值
-       		#2.2# $workbook-> write($row, 3, substr($line,44,13), $format_data);	#输出上限值
-       		$workbook-> write($row, 3, $string[4], $format_data);
-       		#2.2# $workbook-> write($row, 4, substr($line,58,13), $format_data);	#输出下限值
-       		$workbook-> write($row, 4, substr($string[5],0,13), $format_data);
-			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Max
-			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Min
-			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Average
-			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  #输出标准差
-			$workbook-> write_formula($row, 9, "=IF(I".($row+1).">0,(D".($row+1)."-E".($row+1).")/6/I".($row+1).")", $format_data);  #输出CP
-			$workbook-> write_formula($row, 10, "=MIN((D".($row+1)."-H".($row+1)."),(H".($row+1)."-E".($row+1)."))/I".($row+1)."/3", $format_data);  #输出CPK
-       		$row++;
-       		}
-
-       	elsif ($line =~ "\@A-")
+        elsif ($line =~ "\@LIM2" and $line =~ "\@A-DIO" and scalar @string == 6)    # Diode
        	{
        		$subtitle = substr ($line, 24, rindex($line,"\{@LIM") - 24),"\n";
        		#print "\n".$headN."/".$subtitle; 
        		print NLog $headN."/".$subtitle, "\n";
-       		$workbook-> write($row, $col, $headN."/".$subtitle, $format_item);		#输出测试名，多项测试
-			$workbook-> write($row, 1, substr($line,4,3), $format_data);  			#输出TYPE
-       		#2.2# $workbook-> write($row, 3, substr($line,index($line,"\@LIM")+6,13), $format_data);	#输出上限值
+       		$workbook-> write($row, $col, $headN."/".$subtitle, $format_item);		# 输出测试名，多项测试
+			$workbook-> write($row, 1, substr($line,4,3), $format_data);  			# 输出TYPE
        		$workbook-> write($row, 3, $string[4], $format_data);
-       		#2.2# $workbook-> write($row, 4, substr($line,rindex($line,"|")+1,13), $format_data);  		#输出下限值
        		$workbook-> write($row, 4, substr($string[5],0,13), $format_data);
-			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Max
-			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Min
-			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  #输出Average
-			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  #输出标准差
+			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Max
+			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Min
+			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Average
+			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出标准差
 			$workbook-> write_formula($row, 9, "=IF(I".($row+1).">0,(D".($row+1)."-E".($row+1).")/6/I".($row+1).")", $format_data);  #输出CP
 			$workbook-> write_formula($row, 10, "=MIN((D".($row+1)."-H".($row+1)."),(H".($row+1)."-E".($row+1)."))/I".($row+1)."/3", $format_data);  #输出CPK
+
+       		$workbook-> conditional_formatting($row, 5,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'greater than',
+    			 	value    => "=D".($row+1)."-(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+
+       		$workbook-> conditional_formatting($row, 6,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'less than',
+    			 	value    => "=E".($row+1)."+(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+    		
+       		$row++;
+       		}
+        elsif ($line =~ "\@LIM2" and scalar @string == 5)    # single step Volts
+       	{
+       		#print $headN, "\r";
+       		print NLog $headN, "\r";
+       		$workbook-> write($row, $col, $headN, $format_item);					# 输出测试名，单项测试
+			$workbook-> write($row, 1, substr($line,4,3), $format_data);			# 输出TYPE
+       		$workbook-> write($row, 3, $string[3], $format_data);
+       		$workbook-> write($row, 4, substr($string[4],0,13), $format_data);
+			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Max
+			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Min
+			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Average
+			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出标准差
+			$workbook-> write_formula($row, 9, "=IF(I".($row+1).">0,(D".($row+1)."-E".($row+1).")/6/I".($row+1).")", $format_data);  #输出CP
+			$workbook-> write_formula($row, 10, "=MIN((D".($row+1)."-H".($row+1)."),(H".($row+1)."-E".($row+1)."))/I".($row+1)."/3", $format_data);  #输出CPK
+
+       		$workbook-> conditional_formatting($row, 5,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'greater than',
+    			 	value    => "=D".($row+1)."-(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+       		$workbook-> conditional_formatting($row, 6,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'less than',
+    			 	value    => "=E".($row+1)."+(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+    		
+       		$row++;
+       		}
+        elsif ($line =~ "\@LIM3" and scalar @string == 6)     # LCR
+       	{
+       		#print $headN, "\r";
+       		print NLog $headN, "\r";
+       		$workbook-> write($row, $col, $headN, $format_item);								# 输出测试名，单项测试
+			$workbook-> write($row, 1, substr($line,4,3), $format_data);						# 输出TYPE
+       		$workbook-> write($row, 2, substr($line,index($line,"\@LIM")+6,13), $format_data);  # 输出正常值
+       		$workbook-> write($row, 3, $string[4], $format_data);
+       		$workbook-> write($row, 4, substr($string[5],0,13), $format_data);
+			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Max
+			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Min
+			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Average
+			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出标准差
+			$workbook-> write_formula($row, 9, "=IF(I".($row+1).">0,(D".($row+1)."-E".($row+1).")/6/I".($row+1).")", $format_data);  #输出CP
+			$workbook-> write_formula($row, 10, "=MIN((D".($row+1)."-H".($row+1)."),(H".($row+1)."-E".($row+1)."))/I".($row+1)."/3", $format_data);  #输出CPK
+
+			#Hli = 41.25-(41.25-28.05)*0.25
+			#Lli = 28.05+(41.25-28.05)*0.25
+       		$workbook-> conditional_formatting($row, 5,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'greater than',
+    			 	value    => "=D".($row+1)."-(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+       		$workbook-> conditional_formatting($row, 6,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'less than',
+    			 	value    => "=E".($row+1)."+(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+    		
+       		$row++;
+       		}
+
+       	elsif ($line =~ "\@A-")        # Volts
+       	{
+       		$subtitle = substr ($line, 24, rindex($line,"\{@LIM") - 24),"\n";
+       		#print "\n".$headN."/".$subtitle; 
+       		print NLog $headN."/".$subtitle, "\n";
+       		$workbook-> write($row, $col, $headN."/".$subtitle, $format_item);		# 输出测试名，多项测试
+			$workbook-> write($row, 1, substr($line,4,3), $format_data);  			# 输出TYPE
+       		$workbook-> write($row, 3, $string[4], $format_data);
+       		$workbook-> write($row, 4, substr($string[5],0,13), $format_data);
+			$workbook-> write_formula($row, 5, "=MAX(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Max
+			$workbook-> write_formula($row, 6, "=MIN(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Min
+			$workbook-> write_formula($row, 7, "=AVERAGE(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出Average
+			$workbook-> write_formula($row, 8, "=STDEV(L".($row+1).":AAA".($row+1).")", $format_data);  # 输出标准差
+			$workbook-> write_formula($row, 9, "=IF(I".($row+1).">0,(D".($row+1)."-E".($row+1).")/6/I".($row+1).")", $format_data);  #输出CP
+			$workbook-> write_formula($row, 10, "=MIN((D".($row+1)."-H".($row+1)."),(H".($row+1)."-E".($row+1)."))/I".($row+1)."/3", $format_data);  #输出CPK
+
+       		$workbook-> conditional_formatting($row, 5,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'greater than',
+    			 	value    => "=D".($row+1)."-(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+       		$workbook-> conditional_formatting($row, 6,
+    			{
+    				type     => 'cell',
+    			 	criteria => 'less than',
+    			 	value    => "=E".($row+1)."+(D".($row+1)."-E".($row+1).")*0.2",
+    			 	format   => $format_Fcpk,
+    			});
+			
        		$row++;
        		}
     	}close NLog;}
@@ -255,7 +379,7 @@ open (Titles, "<head");
 @Titles = <Titles>;
 while($title = <@Titles>)	# Items
 {
-	$col = 11;	#初始化列数
+	$col = 11;	# 初始化列数
 	$row++;
 	chomp $title;
 	print "extracting $title\n";
@@ -269,7 +393,7 @@ while($title = <@Titles>)	# Items
 		    {
 		    	chomp $line;
 		    	#print substr($line,8,length($line)-11)."\n";
-				if ($line =~ "\@BTEST" and $items == 0 and $counter == 1)	#写SN到Summary中
+				if ($line =~ "\@BTEST" and $items == 0 and $counter == 1)	# 写SN到Summary中
 				{
 				#$summary-> write($col-10, 0, substr($line,8,$SNlength), $format_item);
 				#if(substr($line,9+$SNlength,2) eq "00"){$summary-> write($col-10, 1, "Pass", $format_Pcpk);}
@@ -328,7 +452,7 @@ while($title = <@Titles>)	# Items
 		    	#print substr($title,index($title,"\%")+1,length($title)-index($title,"\%"))."\n";
 		    	#print substr($line,index($line,"\%")+1,length($line)-index($line,"\%")-4)."\n";
 				#print "#"."\n";
-				if ($line =~ "\@BTEST" and $items == 0 and $counter == 1)	#写SN到Summary中
+				if ($line =~ "\@BTEST" and $items == 0 and $counter == 1)	# 写SN到Summary中
 				{
 				#$summary-> write($col-10, 0, substr($line,8,$SNlength), $format_item);
 				#if(substr($line,9+$SNlength,2) eq "00"){$summary-> write($col-10, 1, "Pass", $format_Pcpk);}
@@ -390,10 +514,11 @@ while($title = <@Titles>)	# Items
 			}
 $items++;
 	}
+
 close Title;
 unlink "head";
 $log_report->close();
-print "=> Done.....\n";
+print "\n	>>> Done.....\n\n";
 system 'pause';
 
 exit;
